1390e7b423fa0002e985b16f2c7be00a3d9e7ee9
dcp0006 <dcp0006@alu.medac.es>
2023-05-17 14:18:22 +0200

    Actualizaciones finales

--- src/Ventas/Conexion.java
+++ src/Ventas/Conexion.java
@@ -16,13 +16,13 @@ public static void añadirProducto(int cantidad,String producto,double precio) t
 
 	try {
 		 // Cargar el controlador JDBC
-       Class.forName("com.mysql.cj.jdbc.Driver");
+       Class.forName("org.sqlite.JDBC");
 
        // Establecer la conexión a la base de datos
-       String url = "jdbc:mysql://localhost:3306/prueba";
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
        String user = "root";
        String contraseña = "";
-       Connection conexion = DriverManager.getConnection(url, user, contraseña);
+       Connection conexion = DriverManager.getConnection(url);
        
        //mete los valores en base de datos
 	String consulta="insert into productos values('"+cantidad+"','"+producto+"','"+precio+"')";
@@ -39,13 +39,13 @@ public static void añadirVenta(String producto,int cantidad,double precio) thro
 
 	try {
 		 // Cargar el controlador JDBC
-       Class.forName("com.mysql.cj.jdbc.Driver");
+       Class.forName("org.sqlite.JDBC");
 
        // Establecer la conexión a la base de datos
-       String url = "jdbc:mysql://localhost:3306/prueba";
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
        String user = "root";
        String contraseña = "";
-       Connection conexion = DriverManager.getConnection(url, user, contraseña);
+       Connection conexion = DriverManager.getConnection(url);
        
        //mete los valores en base de datos
 	String consulta="insert into ventas values('"+producto+"','"+cantidad+"','"+precio+"')";
@@ -64,13 +64,13 @@ public static void añadirVenta(String producto,int cantidad,double precio) thro
 public static void eliminarTodosProducto() throws SQLException {
 	try {
 		 // Cargar el controlador JDBC
-       Class.forName("com.mysql.cj.jdbc.Driver");
+       Class.forName("org.sqlite.JDBC");
 
        // Establecer la conexión a la base de datos
-       String url = "jdbc:mysql://localhost:3306/prueba";
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
        String user = "root";
        String contraseña = "";
-       Connection conexion = DriverManager.getConnection(url, user, contraseña);
+       Connection conexion = DriverManager.getConnection(url);
        
        //mete los valores en base de datos
        String consulta="delete from productos";
@@ -86,13 +86,13 @@ public static void eliminarTodosProducto() throws SQLException {
 public static void cargarTablaInven() throws SQLException {
 	try {
 		 // Cargar el controlador JDBC
-       Class.forName("com.mysql.cj.jdbc.Driver");
+       Class.forName("org.sqlite.JDBC");
 
        // Establecer la conexión a la base de datos
-       String url = "jdbc:mysql://localhost:3306/prueba";
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
        String user = "root";
        String contraseña = "";
-       Connection conexion = DriverManager.getConnection(url, user, contraseña);
+       Connection conexion = DriverManager.getConnection(url);
        
        //mete los valores en base de datos
 	String consulta="select * from productos";
@@ -113,13 +113,13 @@ public static void cargarTablaInven() throws SQLException {
 public static void cargarTablaVentas() throws SQLException {
 	try {
 		 // Cargar el controlador JDBC
-       Class.forName("com.mysql.cj.jdbc.Driver");
+       Class.forName("org.sqlite.JDBC");
 
        // Establecer la conexión a la base de datos
-       String url = "jdbc:mysql://localhost:3306/prueba";
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
        String user = "root";
        String contraseña = "";
-       Connection conexion = DriverManager.getConnection(url, user, contraseña);
+       Connection conexion = DriverManager.getConnection(url);
        
        //mete los valores en base de datos
 	String consulta="select * from ventas";
@@ -130,10 +130,41 @@ public static void cargarTablaVentas() throws SQLException {
 		String producto=resultText.getString("producto");
 		double precio=resultText.getDouble("precio");
 		Interface.modeloVentas.addRow(new Object[] {producto,precio});
-		System.out.println(producto+"  "+precio);
+		
 	}
 	} catch (Exception e) {
 		System.out.println("Error carga tabla ventas");
+		
+	}
+}
+public static void cargarTablaClientes() throws SQLException {
+	try {
+		 // Cargar el controlador JDBC
+       Class.forName("org.sqlite.JDBC");
+
+       // Establecer la conexión a la base de datos
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
+       String user = "root";
+       String contraseña = "";
+       Connection conexion = DriverManager.getConnection(url);
+       
+       //mete los valores en base de datos
+	String consulta="select producto from ventas";
+	Statement statement=conexion.createStatement();
+	
+	ResultSet resultText=statement.executeQuery(consulta);
+	while(resultText.next()) {
+		String[] nombre= {"Lucia","Paco","Jordan","Sergio","Jorge"};
+		int ls=nombre.length-1;
+		int li=0;
+		String producto=resultText.getString("producto");
+		String nombre2=nombre[(int) (Math.random()*(ls-li+1)+1)];
+		String correo=nombre2+"@gmail.com";
+		Interface.modeloClientes.addRow(new Object[] {nombre2,producto,correo});
+		System.out.println(nombre2+producto+correo);
+	}
+	} catch (Exception e) {
+		System.out.println("Error carga tabla Clientes");
 		
 	}
 }
@@ -141,13 +172,13 @@ public static void cargarTablaVentas() throws SQLException {
 public static void actualizarProducto(int cantidad,String producto,double precio) throws SQLException {
 	try {
 		 // Cargar el controlador JDBC
-       Class.forName("com.mysql.cj.jdbc.Driver");
+       Class.forName("org.sqlite.JDBC");
 
        // Establecer la conexión a la base de datos
-       String url = "jdbc:mysql://localhost:3306/prueba";
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
        String user = "root";
        String contraseña = "";
-       Connection conexion = DriverManager.getConnection(url, user, contraseña);
+       Connection conexion = DriverManager.getConnection(url);
        
        //mete los valores en base de datos
        String consulta="update productos set cantidad='"+cantidad+"',precio='"+precio+"' where producto='"+producto+"'";
@@ -161,13 +192,13 @@ public static void actualizarProducto(int cantidad,String producto,double precio
 public static void actualizarUsuario(String usuario,String password) throws SQLException {
 	try {
 		 // Cargar el controlador JDBC
-       Class.forName("com.mysql.cj.jdbc.Driver");
+       Class.forName("org.sqlite.JDBC");
 
        // Establecer la conexión a la base de datos
-       String url = "jdbc:mysql://localhost:3306/prueba";
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
        String user = "root";
        String contraseña = "";
-       Connection conexion = DriverManager.getConnection(url, user, contraseña);
+       Connection conexion = DriverManager.getConnection(url);
        
        //mete los valores en base de datos
        String consulta="update usuario set contraseña='"+password+"' where id_usuario='"+usuario+"'";
@@ -182,13 +213,13 @@ public static void actualizarUsuario(String usuario,String password) throws SQLE
 public static void eliminarProducto(Object producto) throws SQLException {
 	try {
 		 // Cargar el controlador JDBC
-       Class.forName("com.mysql.cj.jdbc.Driver");
+       Class.forName("org.sqlite.JDBC");
 
        // Establecer la conexión a la base de datos
-       String url = "jdbc:mysql://localhost:3306/prueba";
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
        String user = "root";
        String contraseña = "";
-       Connection conexion = DriverManager.getConnection(url, user, contraseña);
+       Connection conexion = DriverManager.getConnection(url);
        
        //mete los valores en base de datos
        String consulta="delete from productos where producto='"+ producto+"'";
@@ -203,13 +234,13 @@ public static void registroUsuario(String usuario,String password) throws SQLExc
 	//Iniciar conexion con mysql
 	try {
 		 // Cargar el controlador JDBC
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("org.sqlite.JDBC");
 
         // Establecer la conexión a la base de datos
-        String url = "jdbc:mysql://localhost:3306/prueba";
+        String url ="jdbc:sqlite:src/conexion/conexion.db";
         String user = "root";
         String contraseña = "";
-        Connection conexion = DriverManager.getConnection(url, user, contraseña);
+        Connection conexion = DriverManager.getConnection(url);
         
         //mete los valores en base de datos
 	String consulta="insert into usuarios values('"+usuario+"','"+password+"')";
@@ -224,13 +255,13 @@ public static void registroUsuario(String usuario,String password) throws SQLExc
 public static boolean loginUsuario(String usuario,String password) {
 	try {
 		 // Cargar el controlador JDBC
-       Class.forName("com.mysql.cj.jdbc.Driver");
+       Class.forName("org.sqlite.JDBC");
 
        // Establecer la conexión a la base de datos
-       String url = "jdbc:mysql://localhost:3306/prueba";
+       String url ="jdbc:sqlite:src/conexion/conexion.db";
        String user = "root";
        String contraseña = "";
-       Connection conexion = DriverManager.getConnection(url, user, contraseña);
+       Connection conexion = DriverManager.getConnection(url);
        
        //mete los valores en base de datos
 	String consulta="select * from usuarios where id_usuario='"+usuario+"' and contraseña='"+password+"'";
@@ -252,8 +283,5 @@ public static boolean loginUsuario(String usuario,String password) {
 	}
 	return false;
 }
-public static void main(String[] args) {
-	
-	
-}
+
 }
